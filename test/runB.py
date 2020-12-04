26897383ed6d833c964eb92ff023ab9d31914075
Gabriel <gaba929@gmail.com>
2020-12-03 20:49:20 -0600

    run config

--- test/runB.py
+++ test/runB.py
@@ -3,6 +3,8 @@ process = cms.Process("Rootupler")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 100000
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.options.allowUnscheduled = cms.untracked.bool(True)
 
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
@@ -12,3 +14,26 @@ process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
+
+
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+
+        #MiniAOD UltraLegacy2018 we need parked data
+        '/store/data/Run2018A/Charmonium/MINIAOD/12Nov2019_UL2018_rsb-v1/10000/08F41CB9-8F1F-D44F-A5FC-D17E38328C4C.root',
+
+ )
+)
+
+process.TFileService = cms.Service("TFileService",
+       fileName = cms.string('testParcked-MiniAOD.root'),
+)
+
+#### B Fitter-Tupler 
+process.Btupler = cms.EDAnalyzer('BtoKsMuMu'
+
+    )
+
+process.p = cms.Path(process.Btupler)
