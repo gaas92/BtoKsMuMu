2f2179d639bd06e3a97856547d36f0d50a069f56
Gabriel <gaba929@gmail.com>
2020-12-04 02:13:20 -0600

    parked file test

--- test/Psiks0Rootupler.py
+++ test/Psiks0Rootupler.py
@@ -36,17 +36,17 @@ process.source = cms.Source("PoolSource",
  )
 )
 
-process.triggerSelection = cms.EDFilter("TriggerResultsFilter",
-                                        triggerConditions = cms.vstring('HLT_Dimuon20_Jpsi_Barrel_Seagulls_v*',
-                                                                        'HLT_Dimuon25_Jpsi_v*',
-                                                                        'HLT_DoubleMu4_JpsiTrkTrk_Displaced_v*',
-                                                                        'HLT_DoubleMu4_JpsiTrk_Displaced_v*',
-                                                                        'HLT_DoubleMu4_Jpsi_Displaced_v*'                                   
-                                                                       ),
-                                        hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
-                                        l1tResults = cms.InputTag( "" ),
-                                        throw = cms.bool(False)
-                                        )
+#process.triggerSelection = cms.EDFilter("TriggerResultsFilter",
+#                                        triggerConditions = cms.vstring('HLT_Dimuon20_Jpsi_Barrel_Seagulls_v*',
+#                                                                        'HLT_Dimuon25_Jpsi_v*',
+#                                                                        'HLT_DoubleMu4_JpsiTrkTrk_Displaced_v*',
+#                                                                        'HLT_DoubleMu4_JpsiTrk_Displaced_v*',
+#                                                                        'HLT_DoubleMu4_Jpsi_Displaced_v*'                                   
+#                                                                       ),
+#                                        hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
+#                                        l1tResults = cms.InputTag( "" ),
+#                                        throw = cms.bool(False)
+#                                        )
 
 process.load("myAnalyzers.BtoKsMuMu.Psiks0Rootupler_cfi")
 
@@ -55,11 +55,11 @@ process.TFileService = cms.Service("TFileService",
        fileName = cms.string('Rootuple_Bdtojpiks0_2018UL_MiniAOD.root'),
 )
 
-process.mySequence = cms.Sequence(
-                                   process.triggerSelection *
-                                   process.rootuple
-				   )
-
+#process.mySequence = cms.Sequence(
+#                                   process.triggerSelection *
+#                                   process.rootuple
+#				   )
+#
 #process.p = cms.Path(process.mySequence)
-process.p = cms.Path(process.triggerSelection*process.rootuple)
+process.p = cms.Path(process.rootuple)
 
