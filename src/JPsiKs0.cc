842dea2caadc74cb32e8ac6da5eb2583cdfa8da7
Gabriel <gaba929@gmail.com>
2020-12-20 14:54:48 -0600

    add mc

--- src/JPsiKs0.cc
+++ src/JPsiKs0.cc
@@ -1180,7 +1180,7 @@ void JPsiKs0::printMCtreeUP(const reco::Candidate* daughter, int indent = 0){
         	extraIndent+=1;
 	    }
 		if (mother->pdgId() == daughter->pdgId()) return;
-        if (mother->numberOfMothers()) printMCtreeUP(daughter, indent+extraIndent);
+        if (mother->numberOfMothers() > 1) printMCtreeUP(daughter, indent+extraIndent);
     }
 }
 // ------------ method called once each job just before starting event loop  ------------
