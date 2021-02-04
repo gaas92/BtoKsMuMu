98509aa915b4a8b746945285dda2b43ab9ced798
Gabriel <gaba929@gmail.com>
2021-02-04 12:51:05 -0600

    run my Gen test

--- test/Run_McGenAnalyzer.py
+++ test/Run_McGenAnalyzer.py
@@ -10,7 +10,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
         #fileNames = cms.untracked.vstring( open('my_gen-NF_files.txt').readlines())
-        filenNames = cms.untracked.vstring(
+        fileNames = cms.untracked.vstring(
             ['/eos/user/g/gayalasa/Step0-BPHnoFilters_B0toK0MM/crab_2018-PrivateMC-Step0-BPHnoFilters_B0toK0MM-2021-01-27-19-15/210127_181518/0000/BPH_B0toK0MM_GenNF_1.root']
         )
 		)
