5f125e4ab285d04e661f89b4b01b00d1c9d556a5
Gabriel <gaba929@gmail.com>
2021-02-04 00:28:59 -0600

    get ready for crab data

--- test/Psiks0_BestPA_V0Ext_Rootupler.py
+++ test/Psiks0_BestPA_V0Ext_Rootupler.py
@@ -51,7 +51,7 @@ process.source = cms.Source("PoolSource",
 #                                        )
 
 process.load("myAnalyzers.BtoKsMuMu.Psiks0_BestPA_V0Ext_Rootupler_cfi")
-process.rootuple.isMC = cms.bool(True) # this is only for test
+#process.rootuple.isMC = cms.bool(True) # this is only for test
 
 process.TFileService = cms.Service("TFileService",
 
