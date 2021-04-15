e36c35c616b009e1c66172692ae4f52061b67cc8
Gabriel <gaba929@gmail.com>
2021-04-14 21:36:25 -0500

    my gen nf test

--- test/test_script.py
+++ test/test_script.py
@@ -1,4 +1,4 @@
-files_my_gen = [string for string in open('noProbeFilterDecayFilter_MiniAOD.txt').readlines() if string != ""]
+files_my_gen = [string for string in open('noProbeFilterDecayFilter_MiniAOD.txt').readlines() if len(string) > 10]
 
 for f in files_my_gen:
     if len(f) < 10:
