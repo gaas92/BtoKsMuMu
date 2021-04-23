4b152e19b0420af57f43e9b2379ef8f2de6ae625
Gabriel <gaba929@gmail.com>
2021-04-22 20:56:29 -0500

    my gen nf Full Gen

--- src/JPsiKs0_PVpa_V0Ext.h
+++ src/JPsiKs0_PVpa_V0Ext.h
@@ -191,12 +191,12 @@ private:
   int  run, event;
   int  lumiblock;
   UInt_t trigger;
-  int ngen;
 
 
   TLorentzVector gen_b_p4,gen_jpsi_p4,gen_pion1_p4,gen_pion2_p4,gen_ks0_p4,gen_muon1_p4,gen_muon2_p4;
   TVector3       gen_b_vtx,gen_jpsi_vtx,gen_ks0_vtx;
   float          gen_b_ct, gen_ks0_ct;
+  int ngen;
 
 };
 
