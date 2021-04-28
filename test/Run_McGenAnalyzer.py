f8e1ce68ba9dc3aa525a50cfb64761b892e933d9
Gabriel <gaba929@gmail.com>
2021-04-28 00:52:32 -0500

    Gen resonant

--- test/Run_McGenAnalyzer.py
+++ test/Run_McGenAnalyzer.py
@@ -9,7 +9,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 
 process.source = cms.Source("PoolSource",
-        fileNames = cms.untracked.vstring( open('noProbeFilterDecayFilter_Gen.txt').readlines())
+        fileNames = cms.untracked.vstring( open('myGenFiles/Res_noProbeFilterDecayFilter_Gen.txt').readlines())
         #fileNames = cms.untracked.vstring(
         #    ['/store/mc/RunIIAutumn18DR/BdToK0sMuMu_Mufilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/GEN-SIM-DIGI-RAW/PUPoissonAve20_102X_upgrade2018_realistic_v15-v2/100000/001BF693-83CF-4648-AC6B-43B3EB4EAE30.root']
         #)
@@ -18,7 +18,7 @@ process.source = cms.Source("PoolSource",
 
 
 process.TFileService = cms.Service("TFileService",
-         fileName = cms.string('test_B0Gen_noProbeFilterDecayFilter.root'),                                  
+         fileName = cms.string('test_B0ResGen_noProbeFilterDecayFilter.root'),                                  
 )
 
 #Analyzer
