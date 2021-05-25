42997e3a1971d1acee1aff8f87b1e61710462778
Gabriel <gaba929@gmail.com>
2021-05-25 12:36:55 -0500

    Trigger Studies update

--- src/JPsiKs0_PVpa_V0Ext.cc
+++ src/JPsiKs0_PVpa_V0Ext.cc
@@ -704,7 +704,9 @@ void JPsiKs0_PVpa_V0Ext::analyze(const edm::Event& iEvent, const edm::EventSetup
 	TriggerObj_px->push_back(obj.px());
 	TriggerObj_py->push_back(obj.py());
 	TriggerObj_pz->push_back(obj.pz());
-	TriggerObj_ch->push_back(obj.ch());
+	TriggerObj_ch->push_back(obj.charge());
+    //float IPxy  = iMuon1->muonBestTrack()->dxy(referencePos);
+	//float IPxyE = iMuon1->muonBestTrack()->dxyError();
 	//TriggerObj_IP
 	//TriggerObj_IPE
 
