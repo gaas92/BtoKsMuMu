7018603d6fa6c3cefcf4dc61a9314cecf3ae8940
Gabriel <gaba929@gmail.com>
2021-05-28 18:27:41 -0500

    TnP debug

--- test/Run_TnP.py
+++ test/Run_TnP.py
@@ -114,8 +114,8 @@ process.l1bits=cms.EDProducer("L1TriggerResultsConverter",
 
 process.TnP = cms.EDFilter("TagAndProbeProducer_MC",
         muonIDScaleFactors = cms.int32(0),
-        requireTag = cms.int32(0),
-        verbose = cms.int32(1)
+        requireTag = cms.int32(1),
+        verbose = cms.int32(0)
 )
 
 
