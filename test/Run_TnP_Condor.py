ba6bf778244ed71cd050ca423307fa458579f72c
Gabriel <gaba929@gmail.com>
2021-06-23 15:08:36 -0500

    TnP for condor

--- test/Run_TnP_Condor.py
+++ test/Run_TnP_Condor.py
@@ -95,7 +95,7 @@ process.source = cms.Source("PoolSource",
 
 
 #outname = f'TagAndProbeTrigger_{ "MC" if options.isMC else "RD"}_part{options.input_file[len(options.input_file)-7: len(options.input_file)-4]}.root'
-outname = 'TagAndProbeTrigger_{}_part{}.root'.format('MC' if options.isMC  else 'RD',  str(options.input_file)[len(options.input_file)-7: len(options.input_file)-4])
+outname = 'TagAndProbeTrigger_{}_part{}.root'.format('MC' if options.isMC  else 'RD',  str(options.inputFile)[len(options.inputFile)-7: len(options.inputFile)-4])
 #outname = 'TagAndProbeTrigger_part.root'
 
 process.TFileService = cms.Service("TFileService",
