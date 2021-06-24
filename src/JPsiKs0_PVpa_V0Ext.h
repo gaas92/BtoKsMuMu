020c7578170261c6b1f425f6bebeedc8b2f9c7a1
Gabriel <gaba929@gmail.com>
2021-06-24 01:14:44 -0500

    fix trigger match

--- src/JPsiKs0_PVpa_V0Ext.h
+++ src/JPsiKs0_PVpa_V0Ext.h
@@ -45,6 +45,7 @@
 
 #include "DataFormats/PatCandidates/interface/PackedCandidate.h" // muy importante para MiniAOD
 #include "DataFormats/PatCandidates/interface/PackedGenParticle.h"
+#include "DataFormats/PatCandidates/interface/PackedTriggerPrescales.h"
 
 #include "CondFormats/L1TObjects/interface/L1GtTriggerMenu.h"
 #include "CondFormats/DataRecord/interface/L1GtTriggerMenuRcd.h"
