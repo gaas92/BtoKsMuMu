b5086133f6904b42317ac7a721d16897e35b49bb
Gabriel <gaba929@gmail.com>
2021-06-24 00:15:01 -0500

    Reco Private MC Condor

--- test/Reco_PriivateMC_Condor.py
+++ test/Reco_PriivateMC_Condor.py
@@ -1,5 +1,54 @@
 import FWCore.ParameterSet.Config as cms
 process = cms.Process("Rootuple")
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+'''
+############ Command line args ################
+'''
+
+options = VarParsing('python')
+
+options.register('isRes', False,
+                VarParsing.multiplicity.singleton,
+                VarParsing.varType.bool,
+                "Run Resonant or non resonant MC")
+
+options.register('onlyGen', False,
+                VarParsing.multiplicity.singleton,
+                VarParsing.varType.bool,
+                "Run MC onlyGen level")
+
+options.register('globalTag', '102X_upgrade2018_realistic_v12',
+                VarParsing.multiplicity.singleton,
+                VarParsing.varType.string,
+                "Set global tag"
+) 
+
+options.register('maxE', -1,
+                VarParsing.multiplicity.singleton,
+                VarParsing.varType.int,
+                "Maximum number of events"
+)
+
+options.register('tg', 'PrivateMC_Condor_Result',
+                VarParsing.multiplicity.singleton,
+                VarParsing.varType.string,
+                "tag for outputfile"
+)
+
+options.register('inputFile', 'noProbeFilterDecayFilter_MiniAOD1_0.txt',
+                VarParsing.multiplicity.singleton,
+                VarParsing.varType.string,
+                "input txt file"
+)
+
+options.register('saveInSync', False,
+                VarParsing.multiplicity.singleton,
+                VarParsing.varType.bool,
+                "save result file in sync")
+
+options.parseArguments()
+
 
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
@@ -15,61 +64,30 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data')
 process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.options.allowUnscheduled = cms.untracked.bool(True)
-#process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
 #process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(2000))
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-#files_my_gen = [string for string in open('myGenFiles/noProbeFilterDecayFilter_MiniAOD2_4.txt').readlines() if len(string) > 10]
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(options.maxE))
+files_my_gen = [string for string in open('myGenFiles/'+options.inputFile).readlines() if len(string) > 10]
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(#files_my_gen
-
-        #Parked Data
-        '/store/data/Run2018A/ParkingBPH1/MINIAOD/05May2019-v1/00000/2129F080-982D-A649-8D20-4944620E99A6.root',
-        '/store/data/Run2018A/ParkingBPH1/MINIAOD/05May2019-v1/00000/12C659FD-D961-6640-9C3D-48CD12A0D033.root', 
-        '/store/data/Run2018A/ParkingBPH1/MINIAOD/05May2019-v1/00000/05030414-6C93-DC46-AD09-68D76E2FB466.root'
-        # Test on MC 
-        #'/store/mc/RunIIAutumn18MiniAOD/BdToK0sMuMu_Mufilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/MINIAODSIM/PUPoissonAve20_BParking_102X_upgrade2018_realistic_v15-v2/100000/17621299-2380-064D-A5E9-B78072D95A7D.root'   
-
-        #MiniAOD UltraLegacy2018
-        #'/store/data/Run2018A/Charmonium/MINIAOD/12Nov2019_UL2018_rsb-v1/10000/08F41CB9-8F1F-D44F-A5FC-D17E38328C4C.root',
-
-        #MiniAOD UltraLegacy2017
-        #'/store/data/Run2017F/Charmonium/MINIAOD/09Aug2019_UL2017-v1/20000/00BACB48-9B0F-8F48-A68B-2F08A3E9E681.root',
-
-        #MiniAOD UltraLegacy2016
-        #'/store/data/Run2016G/Charmonium/MINIAOD/21Feb2020_UL2016-v1/30000/00013A18-278D-5B48-9BEF-1083A8F5C9D7.root',
-        
+    fileNames = cms.untracked.vstring(files_my_gen
  )
 )
 
-#process.triggerSelection = cms.EDFilter("TriggerResultsFilter",
-#                                        triggerConditions = cms.vstring('HLT_Dimuon20_Jpsi_Barrel_Seagulls_v*',
-#                                                                        'HLT_Dimuon25_Jpsi_v*',
-#                                                                        'HLT_DoubleMu4_JpsiTrkTrk_Displaced_v*',
-#                                                                        'HLT_DoubleMu4_JpsiTrk_Displaced_v*',
-#                                                                        'HLT_DoubleMu4_Jpsi_Displaced_v*'                                   
-#                                                                       ),
-#                                        hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
-#                                        l1tResults = cms.InputTag( "" ),
-#                                        throw = cms.bool(False)
-#                                        )
 
 process.load("myAnalyzers.BtoKsMuMu.Psiks0_BestPA_V0Ext_Rootupler_cfi")
-process.rootuple.isMC = cms.bool(False) # this is only for test
-process.rootuple.isRes = cms.bool(False)
-process.rootuple.OnlyGen = cms.bool(False)
+process.rootuple.isMC = cms.bool(True) # this is only for test
+process.rootuple.isRes = cms.bool(options.isRes)
+process.rootuple.OnlyGen = cms.bool(options.OnlyGen)
 process.rootuple.GenParticles = cms.InputTag("prunedGenParticles") 
+
+
+outname = 'PrivateMC_{}_{}_part{}.root'.format('Res' if options.isRes  else 'notRes', 'Gen' if options.onlyGen else 'Reco' , str(options.inputFile)[len(options.inputFile)-7: len(options.inputFile)-4])
+
 
 process.TFileService = cms.Service("TFileService",
 
-       fileName = cms.string('Rootuple_BdtoMuMuks0_PARKED_Bpa_PVExt_MiniAOD.root'),
-       #fileName = cms.string('Rootuple_BdtoMuMuks0_PARKED_Bpa_PVExt_MiniAOD_GEN_24.root'),
+       fileName = cms.string(outname),
 )
 
-#process.mySequence = cms.Sequence(
-#                                   process.triggerSelection *
-#                                   process.rootuple
-#				   )
-#
-#process.p = cms.Path(process.mySequence)
 process.p = cms.Path(process.rootuple)
  
