f42a23e91b9e47afc809d33a62042a4e37aeb540
Gabriel <gaba929@gmail.com>
2021-07-20 14:52:54 -0500

    Trigger debugging again

--- src/JPsiKs0_PVpa_V0Ext.cc
+++ src/JPsiKs0_PVpa_V0Ext.cc
@@ -1004,22 +1004,24 @@ void JPsiKs0_PVpa_V0Ext::analyze(const edm::Event& iEvent, const edm::EventSetup
 				if (abs(iMuon1->eta()) > 1.5) std::cout << "muon1 eta: " << iMuon1->eta() << std::endl;
 				std::cout<< " Muon 1 matched " << triggerName << std::endl;
 				std::cout<< " Muon 1 pT: " << iMuon1->pt() << ", IP: " << abs(iMuon1->track()->dxy(referencePos)/iMuon1->track()->dxyError())<< std::endl;
-				/*
+				
 				if (iMuon1->triggerObjectMatches().size()!=0){
-					std::cout << "muon1 match inside . . ." << std::endl;
 					for(size_t k=0; k<iMuon1->triggerObjectMatches().size();k++){
-						if(iMuon1->triggerObjectMatch(k)!=0 && iMuon1->triggerObjectMatch(k)->hasPathName(triggerName.c_str(),true,true)){
 
+						if(iMuon1->triggerObjectMatch(k)!=0 && iMuon1->triggerObjectMatch(k)->hasPathName(triggerName.c_str(),true,true)){
+							std::cout << "muon1 match inside . . ." << std::endl;
+							/*
                             float dr=TMath::Sqrt(pow(iMuon1->triggerObjectMatch(i)->eta()-iMuon1->eta(),2.)+pow(iMuon1->triggerObjectMatch(i)->phi()-iMuon1->phi(),2.));
                             float dpt=(iMuon1->triggerObjectMatch(i)->pt()-iMuon1->pt())/iMuon1->triggerObjectMatch(i)->pt();
 				            if (abs(iMuon1->eta()) > 1.5){
 								 std::cout << "muon1 eta inside: " << iMuon1->eta() << std::endl;
 								 std::cout << "match obj eta: " << iMuon1->triggerObjectMatch(i)->eta() << std::endl;
 							}
+							*/
 						}
 					}
 				}
-				*/
+				
 			} 
 			//if(iMuon2->triggerObjectMatchByPath(triggerName)!=nullptr){ 
 			if(iMuon2->triggered(triggerName.c_str())){ 
