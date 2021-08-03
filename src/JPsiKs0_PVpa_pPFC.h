20e6640029edd4a46d2f0f1a1cef48152e73181a
Gabriel <gaba929@gmail.com>
2021-08-03 15:31:24 -0500

    add trigger info ... again

--- src/JPsiKs0_PVpa_pPFC.h
+++ src/JPsiKs0_PVpa_pPFC.h
@@ -136,8 +136,9 @@ private:
   std::vector<bool>        *mu1soft, *mu2soft, *mu1tight, *mu2tight;  
   std::vector<bool>        *mu1PF, *mu2PF, *mu1loose, *mu2loose;  
 
+  std::vector<int>         *muon1Trg, *muon2Trg;
+
   //Trigger save non-sense 
-  std::vector<int>         *just_test;
   std::vector<int>         *mu1_HLT_Mu7_IP4, *mu1_HLT_Mu8_IP3, *mu1_HLT_Mu8_IP5, *mu1_HLT_Mu8_IP6, *mu1_HLT_Mu8p5_IP3p5;
   std::vector<int>         *mu1_HLT_Mu9_IP0, *mu1_HLT_Mu9_IP3, *mu1_HLT_Mu9_IP4, *mu1_HLT_Mu9_IP5, *mu1_HLT_Mu9_IP6, *mu1_HLT_Mu10p5_IP3p5, *mu1_HLT_Mu12_IP6;
   std::vector<int>         *mu2_HLT_Mu7_IP4, *mu2_HLT_Mu8_IP3, *mu2_HLT_Mu8_IP5, *mu2_HLT_Mu8_IP6, *mu2_HLT_Mu8p5_IP3p5;
