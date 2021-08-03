556cfc8029d8ade30677721a6800ed871902df4f
Gabriel <gaba929@gmail.com>
2021-08-03 18:39:47 -0500

    add trigger info ... again

--- src/ObjVsMuon_tupler.cc
+++ src/ObjVsMuon_tupler.cc
@@ -158,9 +158,9 @@ void ObjVsMuon_tupler::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
     }
 	  if(!isTriggerMuon) continue;
 
-	  //float obj_pt = obj.px();
-	  //float obj_eta = obj.py();
-	  //float obj_phi = obj.pz();
+	  float obj_pt = obj.pt();
+	  float obj_eta = obj.eta();
+	  float obj_phi = obj.phi();
 	  //TriggerObj_pt->push_back(obj_pt);
 	  //TriggerObj_eta->push_back(obj_phi);
 	  //TriggerObj_phi->push_back(obj_eta);
