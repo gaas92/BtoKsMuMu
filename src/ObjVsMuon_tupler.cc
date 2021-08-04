f19f59be370625ab0ba4439b958ea83e91664f85
Gabriel <gaba929@gmail.com>
2021-08-03 20:58:30 -0500

    add trigger info ... again

--- src/ObjVsMuon_tupler.cc
+++ src/ObjVsMuon_tupler.cc
@@ -120,7 +120,7 @@ void ObjVsMuon_tupler::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
   regex txt_regex_path("HLT_Mu[0-9]+_IP[0-9]_part[0-9].*");
   const edm::TriggerNames &names = iEvent.triggerNames(*triggerBits);
   if (verbose) {cout << "\n ==== TRIGGER PATHS ==== " << endl;}
-
+  cout << " x x x" << TriggerObj_pt->size() << endl;
   //look and fill the BParked Trigger Objects ... 
   
   //emulate BParking MuonTriggerSelector 
@@ -165,7 +165,7 @@ void ObjVsMuon_tupler::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
 	  float obj_pt = obj.pt();
 	  float obj_eta = obj.eta();
 	  float obj_phi = obj.phi();
-	  TriggerObj_pt->push_back(obj_pt);
+	  //TriggerObj_pt->push_back(obj_pt);
 	  //TriggerObj_eta->push_back(obj_phi);
 	  //TriggerObj_phi->push_back(obj_eta);
 	  //float obj_ch = obj.charge();
