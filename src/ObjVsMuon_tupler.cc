1958a4ef8e7eb6f77c32756e783f9426d99956be
Gabriel <gaba929@gmail.com>
2021-08-04 17:06:21 -0500

    add trigger info ... again

--- src/ObjVsMuon_tupler.cc
+++ src/ObjVsMuon_tupler.cc
@@ -309,7 +309,7 @@ void ObjVsMuon_tupler::analyze(const edm::Event& iEvent, const edm::EventSetup& 
   for(edm::View<pat::Muon>::const_iterator iMuon = thePATMuonHandle->begin(); iMuon != thePATMuonHandle->end(); ++iMuon) {
 
     for(auto tag : triggerTags) {
-      std::string triggerName = tag + "_part*_v*"; 
+      std::string triggerName = "HLT_" + tag + "_part*_v*"; 
       //std::cout << "checking trigger :" << triggerName << std::endl;
 			if(iMuon->triggered(triggerName.c_str())){ 
         std::cout << "Muon Fired !! " << triggerName << std::endl;
